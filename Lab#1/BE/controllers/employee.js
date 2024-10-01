fd57dfa4d76cd6b61eb3ad90fdf265066c9c7390
Muhammad Amr <42827260+MohammadAmr20@users.noreply.github.com>
2024-10-01 15:28:47 +0300

    Finished Functionalities

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -1,6 +1,4 @@
-const employee = [
-  { id: '1', name: 'Mohamed Sayed' },
-];
+const employee = [{ id: "1", name: "Mohamed Sayed" }];
 
 exports.getEmployees = async (req, res, next) => {
   res.status(200).json({ data: employee });
@@ -8,8 +6,15 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+  const id = req.params.id;
+  const idx = employee.findIndex((el) => id === el.id);
+  employee.splice(idx, 1);
+  res.status(200).json({ data: employee });
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  const { id, name } = req.body;
+  employee.push({ id, name });
+  res.status(200).json({ data: employee });
 };
--- Lab#1/FE/index.js
+++ Lab#1/FE/index.js
@@ -1,52 +1,77 @@
+const API_URL = "http://localhost:3000/api/v1/employee";
+
 function fetchEmployees() {
-  fetch('http://localhost:3000/api/v1/employee')
-    .then(response => response.json())
-    .then(data => {
-      const tableBody = document.getElementById('dataTable')
-      tableBody.innerHTML = ''
-      const list = data.data
-      list.forEach(item => {
-        const row = document.createElement('tr')
-        const idCell = document.createElement('td')
-        idCell.textContent = item.id
-        row.appendChild(idCell)
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+      const tableBody = document.getElementById("dataTable");
+      tableBody.innerHTML = "";
+      const list = data.data;
+      list.forEach((item) => {
+        const row = document.createElement("tr");
+        const idCell = document.createElement("td");
+        idCell.textContent = item.id;
+        row.appendChild(idCell);
 
-        const nameCell = document.createElement('td')
-        nameCell.textContent = item.name
-        row.appendChild(nameCell)
+        const nameCell = document.createElement("td");
+        nameCell.textContent = item.name;
+        row.appendChild(nameCell);
 
-        const deleteCell = document.createElement('td')
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.classList.add('btn', 'btn-danger', 'btn-sm');
+        const deleteCell = document.createElement("td");
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("btn", "btn-danger", "btn-sm");
         deleteCell.appendChild(deleteButton);
+        deleteButton.addEventListener("click", () => deleteEmployee(item.id));
+        row.appendChild(deleteCell);
 
-        row.appendChild(deleteCell)
-
-        tableBody.appendChild(row)
-      })
+        tableBody.appendChild(row);
+      });
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
 }
 
 // TODO
 // add event listener to submit button
-
+const submitButton = document.getElementById("submit-btn");
+submitButton.addEventListener("click", createEmployee);
 // TODO
 // add event listener to delete button
-
 // TODO
-function createEmployee (){
+function createEmployee() {
   // get data from input field
   // send data to BE
   // call fetchEmployees
+  const name = document.getElementById("name").value;
+  const id = document.getElementById("id").value;
+  fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id, name }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      fetchEmployees();
+    })
+    .catch((error) => console.error(error));
+  console.log(name);
 }
 
 // TODO
-function deleteEmployee (){
+function deleteEmployee(id) {
   // get id
   // send id to BE
   // call fetchEmployees
+  fetch(`${API_URL}/${id}`, { method: "DELETE" })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      fetchEmployees();
+    });
 }
 
-fetchEmployees()
+fetchEmployees();
